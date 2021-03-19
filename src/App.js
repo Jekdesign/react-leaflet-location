23f6a0c7996edbecc5813424a8911732b68b4c6a
jvalsin <jvalsin@systra.com>
2021-03-19 21:39:11 +0100

    update states and clean code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "antd/dist/antd.css";
 import "./App.css";
 import { MapContainer, Marker, Popup, Tooltip, TileLayer } from "react-leaflet";
 import axios from "axios";
@@ -6,10 +7,17 @@ import { Tabs } from "antd";
 
 const { TabPane } = Tabs;
 class App extends React.Component {
-  state = {
-    coordonates: [],
-    locationData: [],
-  };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      coordonates: [],
+      locationData: [],
+      locationTypeData: [],
+      organizationGroupData: [],
+      addressData: []
+    };
+  }
 
   componentDidMount() {
     const url = "http://ms-location.local/v1/location/coordonates";
@@ -31,12 +39,16 @@ class App extends React.Component {
       .then((response) => {
         const locationData = response.data._item;
         this.setState({ locationData });
-        console.log(locationData);
+        this.setState({ locationTypeData: locationData.locationType });
+        this.setState({ organizationGroupData: locationData.organizationGroup });
+        this.setState({ addressData: locationData.address });
       })
       .catch((error) => {
         console.log(error);
       });
   }
+
+
 
   render() {
     return (
@@ -57,23 +69,64 @@ class App extends React.Component {
             }}
           >
             <Popup>
-              <p>{this.state.locationData.name}</p>
               <div className="card-container">
                 <Tabs type="card">
-                  <TabPane tab="Tab Title 1" key="1">
-                    <p>Content of Tab Pane 1</p>
-                    <p>Content of Tab Pane 1</p>
-                    <p>Content of Tab Pane 1</p>
+                  <TabPane tab="Tab 1" key="1">
+                    <p>{this.state.locationData.name}</p>
+                    <p>{this.state.locationData.name}</p>
+                    <p>{this.state.locationData.code}</p>
+                    <p>{this.state.locationData.status}</p>
+                    <p>{this.state.locationTypeData.name}</p>
+                    <p>{this.state.locationData.locationOccupancy}</p>
+                    <p>{this.state.organizationGroupData.name}</p>
+                    <p>{this.state.organizationGroupData.reference}</p>
                   </TabPane>
-                  <TabPane tab="Tab Title 2" key="2">
-                    <p>Content of Tab Pane 2</p>
-                    <p>Content of Tab Pane 2</p>
-                    <p>Content of Tab Pane 2</p>
+                  <TabPane tab="Address" key="2">
+                    <p>{this.state.addressData.formatted}</p>
                   </TabPane>
                   <TabPane tab="Tab Title 3" key="3">
-                    <p>Content of Tab Pane 3</p>
-                    <p>Content of Tab Pane 3</p>
-                    <p>Content of Tab Pane 3</p>
+                    <p>
+                      totalContractualArea :{" "}
+                      {this.state.locationData.totalContractualArea}
+                    </p>
+                    <p>
+                      totalSubletArea :{" "}
+                      {this.state.locationData.totalSubletArea}
+                    </p>
+                    <p>
+                      remainingContractualArea :{" "}
+                      {this.state.locationData.remainingContractualArea}
+                    </p>
+                    <p>
+                      effectiveAreaAvailable :{" "}
+                      {this.state.locationData.effectiveAreaAvailable}
+                    </p>
+                    <p>nbDeskMax : {this.state.locationData.nbDeskMax}</p>
+                    <p>
+                      ratioEffectiveAreaNbDeskMax :{" "}
+                      {this.state.locationData.ratioEffectiveAreaNbDeskMax}
+                    </p>
+                    <p>
+                      nbDeskInstalled :{" "}
+                      {this.state.locationData.nbDeskInstalled}
+                    </p>
+                    <p>
+                      ratioEffectiveAreaNbDeskInstalled :{" "}
+                      {
+                        this.state.locationData
+                          .ratioEffectiveAreaNbDeskInstalled
+                      }
+                    </p>
+                    <p>nbDeskUsed : {this.state.locationData.nbDeskUsed}</p>
+                    <p>
+                      ratioEffectiveAreaNbDeskUsed :{" "}
+                      {this.state.locationData.ratioEffectiveAreaNbDeskUsed}
+                    </p>
+                    <p>nbPerson : {this.state.locationData.nbPerson}</p>
+                    <p>
+                      ratioEffectiveAreaNbPerson :{" "}
+                      {this.state.locationData.ratioEffectiveAreaNbPerson}
+                    </p>
                   </TabPane>
                 </Tabs>
               </div>
