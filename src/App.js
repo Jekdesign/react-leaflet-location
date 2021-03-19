d516cf1ba0c4fa36f0049821d0ae302a6bf97876
jvalsin <jvalsin@systra.com>
2021-03-19 12:15:52 +0100

    update interactive map with the call of the APIs for marker and popup

--- src/App.js
+++ src/App.js
@@ -1,37 +1,74 @@
 import React from "react";
-import './App.css';
-import { Map, MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-//import * as parkData from './data/location.json'
-import * as parkData from './data/location2.json'
+import "./App.css";
+import { MapContainer, Marker, Popup, Tooltip, TileLayer } from "react-leaflet";
+import axios from "axios";
+import { Tabs } from "antd";
+class App extends React.Component {
+  state = {
+    coordonates: [],
+    locationData: [],
+  };
 
-function App() {
-  const [activePark, setActivePark] = React.useState(null);
+  componentDidMount() {
+    const url = "http://ms-location.local/v1/location/coordonates";
+    axios
+      .get(url)
+      .then((response) => {
+        const coordonates = response.data._embedded;
+        this.setState({ coordonates });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
-  return (
-    <MapContainer center={[51, 5]} zoom={5}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      />
+  getLocationData(uuid) {
+    const url2 = `http://ms-location.local/v1/location/${uuid}`;
+    axios
+      .get(url2)
+      .then((response) => {
+        const locationData = response.data._item;
+        this.setState({ locationData });
+        console.log(locationData);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
-      {parkData.features.map(park => (
-        <Marker
-          key={park.properties.PARK_ID}
-          position={[
-            park.geometry.coordinates[1],
-            park.geometry.coordinates[0]
-          ]}
-        >
-        <Popup >
-          <div>
-            <h2>{park.properties.NAME}</h2>
-            <p>{park.properties.DESCRIPTIO}</p>
-          </div>
-        </Popup>
-        </Marker>
-      ))}
-    </MapContainer>
-  );
+  render() {
+    return (
+      <MapContainer center={[51, 5]} zoom={5}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+
+        {this.state.coordonates.map((park) => (
+          <Marker
+            key={park.uuid}
+            position={[park.address.lat, park.address.lng]}
+            eventHandlers={{
+              click: () => {
+                this.getLocationData(park.uuid);
+              },
+            }}
+          >
+            <Popup>
+              <p>{this.state.locationData.name}</p>
+            </Popup>
+
+            <Tooltip>
+              <div>
+                <h2>{park.name}</h2>
+                <p>{park.address.formatted}</p>
+              </div>
+            </Tooltip>
+          </Marker>
+        ))}
+      </MapContainer>
+    );
+  }
 }
 
 export default App;
